02d379b4213db5709e4c0b7742afef30a7f04c3d
Israel David <77063876+israel1281@users.noreply.github.com>
2022-02-07 20:17:23 +0100

    first commit

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { motion } from "framer-motion"
+import { animateScroll as scroll } from "react-scroll"
+import MenuIcon from "../Menu/MenuIcon/MenuIcon"
+import "./nav.css"
+
+
+const Nav = ({ menuOpen, setMenuOpen }) => {
+  const handleClick = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className={"nav-wrapper " + (menuOpen && "menuActive")}>
+      <motion.div
+        id="navbar"
+        className={"nav-container"}
+        initial={{
+          opacity: 0,
+        }}
+        animate={{
+          opacity: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+      >
+        <span
+          className={"logo " + (menuOpen && "menuActive")}
+          onClick={() => scroll.scrollToTop()}
+        >
+           Igbigbi .
+        </span>
+
+        <MenuIcon handleClick={handleClick} menuOpen={menuOpen} />
+      </motion.div>
+    </div>
+  );
+};
+
+export default Nav;
