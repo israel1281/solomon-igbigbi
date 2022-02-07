97aaff3f3504b38be9ebef32dcd954c116498ced
Israel David <77063876+israel1281@users.noreply.github.com>
2022-02-07 19:51:40 +0100

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,117 @@
+export const services = [
+  {
+    title: "Frontend Development",
+    iconSrc:
+      "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "Sass",
+      "ReactJs",
+      "NextJs",
+      "Tailwind-css",
+      "Material-UI",
+      "Bootstrap",
+      "Firebase",
+      "Figma",
+      "Canva",
+    ],
+  },
+  {
+    title: "MERN Stack Development",
+    iconSrc: "https://cdn.worldvectorlogo.com/logos/mern.svg",
+    tags: [
+      "MongoDB",
+      "ExpressJs",
+      "ReactJs",
+      "NodeJs",
+      "REST",
+      "Typescript",
+      "NextJs",
+      "Firebase",
+    ],
+  },
+  {
+    title: "Technologies",
+    iconSrc:
+      "https://img.icons8.com/external-flatart-icons-flat-flatarticons/2x/external-excel-taxes-flatart-icons-flat-flatarticons.png",
+    tags: [
+      "SQL",
+      "Sequelize",
+      "Docker",
+      "React-Redux",
+      "Typescript",
+      "Git",
+      "Aws",
+      "Fastify",
+    ],
+  },
+  {
+    title: "Ecommerce Development",
+    iconSrc: "https://img.icons8.com/office/344/bank-cards.png",
+    tags: ["ReactJs", "Typescript", "NextJs", "Stripe", "MongoDB", "ExpressJs"],
+  },
+];
+export const portfolioList = [
+  {
+    id: "featured",
+    title: "Featured",
+  },
+  {
+    id: "frontend",
+    title: "Front-End",
+  },
+  {
+    id: "fullstack",
+    title: "Full-Stack",
+  },
+];
+
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "Pace Africa",
+    img: "https://i.ibb.co/H2kWVq6/Screenshot-20220201-233957.jpg",
+    repositoryUrl: "",
+    liveUrl: "https://pace.africa",
+    tagline: "Recruitment App",
+  },
+  {
+    id: 2,
+    title: "Evisit",
+    img: " https://i.ibb.co/1ZJ2nL4/Screenshot-20220201-234226.jpg  ",
+    repositoryUrl: "",
+    liveUrl: "https://evisit.pro",
+    tagline: "Visitors Management System",
+  },
+];
+
+export const frontendPortfolio = [
+  {
+    id: 1,
+    title: "Pace Africa",
+    img: "https://i.ibb.co/H2kWVq6/Screenshot-20220201-233957.jpg",
+    repositoryUrl: "",
+    liveUrl: "https://pace.africa",
+    tagline: "Recruitment App",
+  },
+  {
+    id: 2,
+    title: "Evisit",
+    img: " https://i.ibb.co/1ZJ2nL4/Screenshot-20220201-234226.jpg  ",
+    repositoryUrl: "",
+    liveUrl: "https://evisit.pro",
+    tagline: "Visitors Management System",
+  },
+];
+
+export const fullstackPortfolio = [
+  {
+    id: 1,
+    title: "Evisit",
+    img: " https://i.ibb.co/1ZJ2nL4/Screenshot-20220201-234226.jpg  ",
+    repositoryUrl: "",
+    liveUrl: "https://evisit.pro",
+    tagline: "Visitors Management System",
+  },
+];
