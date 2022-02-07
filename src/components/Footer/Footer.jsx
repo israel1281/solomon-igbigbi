d28969cd4ca29139578532e4805d199776ec6359
Israel David <77063876+israel1281@users.noreply.github.com>
2022-02-07 20:14:07 +0100

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./footer.css"
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <span className="footer-info">
+        &copy; Developer: Solomon Igbigbi | 2022
+      </span>
+    </div>
+  )
+}
